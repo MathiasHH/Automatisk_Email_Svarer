01cc34ec053f1f8340b36d4bf133dd11d88df7bd
MathiasHH <mathiashafstadhetle@hotmail.no>
2024-10-02 18:35:12 +0200

    Oppdater __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
 #Skal initialisere alle klassene på en gang
+from .email_answerer import EmailAnswerer
+from .config import EMAIL_USER, EMAIL_PASS
+
+def start_email_bot():
+    answerer = EmailAnswerer(EMAIL_USER, EMAIL_PASS)
+    answerer.process_emails()
+
+if __name__ == "__main__":
+    start_email_bot()
